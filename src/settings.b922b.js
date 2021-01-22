50a2eb4e67c32780c5d95d49afa9ec217c47808d
ngoquoctruong <ngoquocturong302@gmail.com>
2021-01-22 13:11:08 +0700

    Updated

---
+++ src/settings.b922b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/landing.fire",orientation:"",jsList:[],bundleVers:{resources:"4d83a",internal:"620a7",main:"cbb9a"}};
